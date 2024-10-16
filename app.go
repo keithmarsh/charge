9bd1652b9076511af321607e1c03882ac1d04def
Keith Marsh <github@keithmarsh.com>
2024-10-16 23:03:07 +0100

    Test upload

--- app.go
+++ app.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"embed"
+	"encoding/csv"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 //go:embed templates/*
@@ -27,7 +30,36 @@ func main() {
 
 		t.ExecuteTemplate(w, "index.html.tmpl", data)
 	})
+	http.HandleFunc("/upload", uploadHandler)
 
 	log.Println("listening on", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+const MAX_UPLOAD_SIZE = 16 * 1024 // 16K
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
+	if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
+		http.Error(w, "The uploaded file is too big. Please choose an file that's less than 1MB in size", http.StatusBadRequest)
+		return
+	}
+	csvRdr := csv.NewReader(r.Body)
+	csv, err := csvRdr.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for ix, line := range csv {
+		if ix > 0 {
+			t := line[1]
+			wh, _ := strconv.ParseFloat(line[25], 32)
+			if wh > 0.0 {
+				fmt.Fprintf(w, "%s %f", t, wh)
+			}
+		}
+	}
 }
